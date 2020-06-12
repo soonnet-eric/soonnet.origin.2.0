83209a0d89f3546fa8433fbcf7bdfcbd35e6e37a
Eric <Eric_Chong@SOONEST_HK>
2020-06-12 16:14:43 +0800

    20200612

---
+++ frontdesk_stage2_20200612_v1/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+  /* 目錄動效
+   * -------------------------------------------------- */
+  $(document).on("scroll", function () {
+    if ($(document).scrollTop() > 0) {
+      $("header").addClass("shrink");
+    } else {
+      $("header").removeClass("shrink");
+    }
+  });
+	
+});
